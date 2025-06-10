fa236720899091de0c1a5b980721ee4dc09ab9fc
Brody <121767457+PAN-DA-BOI@users.noreply.github.com>
2025-06-10 16:33:55 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,8 +13,12 @@ echo "removing desktop enviroment"
 
 #get python up and working
 echo "Installing python"
+sudo apt install -y python3 python3-pip
+pip3 install PyQt5
 
 
-#open up ssh on 2220
-echo "Installing open ssh"
-echo "port: 2220"
+# Open up SSH on port 2220
+echo "Installing OpenSSH"
+sudo apt install openssh-server -y
+sudo sed -i 's/#Port 22/Port 2220/' /etc/ssh/sshd_config
+sudo systemctl restart ssh
