1aebe9f09a7f81f3b7ea9854c527c4c65b098929
Brody <121767457+PAN-DA-BOI@users.noreply.github.com>
2025-08-26 16:57:45 -0600

    updated arduino boards

--- atmega328_boards/HUD/code/HUD_code/HUD_code.ino
+++ atmega328_boards/HUD/code/HUD_code/HUD_code.ino
@@ -1,11 +1,10 @@
 #include "arduino_secrets.h"
-
 #include "Arduino.h"
 #include "uRTCLib.h"
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <dht.h>  
+#include <dht.h>
 #include "gasses.h"
 
 // Temp/humidity
@@ -22,18 +21,19 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 dht DHT;
 uRTCLib rtc(0x68);
 int sensorValue;
-
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+// Timer variables
+unsigned long previousMillis = 0;
+const long interval = 300000; // 5 minutes in milliseconds
 
 void setup() {
   Serial.begin(9600);
   rtc.set(0, 56, 12, 2, 14, 4, 25);
-
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
     for (;;);
   }
-
   display.clearDisplay();
   display.setCursor(0, 28);
   display.println("Warming gas sensor, please wait");
@@ -43,10 +43,10 @@ void setup() {
 
 void loop() {
   display.clearDisplay();
-
   int readData = DHT.read11(outPin);
   sensorValue = analogRead(MQ2pin);
 
+  // Display logic (unchanged)
   if (sensorValue > Threshold) {
     display.clearDisplay();
     display.drawBitmap(0, 0, gasses.gas1(), 128, 64, WHITE);
@@ -57,22 +57,18 @@ void loop() {
     display.display();
     delay(500);
   }
-
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
-
   display.setCursor(0, 0);
   display.print("Temp: ");
   display.print((DHT.temperature * 9.0 / 5.0) + 32.0);
   display.println((char)247); // Degree symbol
   display.println("F");
-
   display.setCursor(0, 16);
   display.print("Humidity: ");
   display.print(DHT.humidity);
   display.println("%");
-
   display.setCursor(0, 32);
   display.print("Time: ");
   display.print(rtc.hour());
@@ -80,7 +76,6 @@ void loop() {
   display.print(rtc.minute());
   display.print(":");
   display.println(rtc.second());
-
   display.setCursor(0, 48);
   display.print("Date: ");
   display.print(rtc.day());
@@ -88,7 +83,17 @@ void loop() {
   display.print(rtc.month());
   display.print("/");
   display.println(rtc.year());
-
   display.display();
+
+  // Send data every 5 minutes
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    // Format: "Time,Temp,Humidity"
+    String data = String(rtc.hour()) + ":" + String(rtc.minute()) + "," +
+                  String((DHT.temperature * 9.0 / 5.0) + 32.0) + "," +
+                  String(DHT.humidity);
+    Serial.println(data); // Send to Arduino 2
+  }
   delay(1000);
 }
--- atmega328_boards/blackbox/code/BLACKBOX/BLACKBOX.ino
+++ atmega328_boards/blackbox/code/BLACKBOX/BLACKBOX.ino
@@ -2,61 +2,27 @@
 #include <SD.h>
 
 File myFile;
-
-// change this to match your SD shield or module;
 const int chipSelect = 10;
 
-void setup()
-{
-  // Open serial communications and wait for port to open:
+void setup() {
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
 
-
-  Serial.print("Initializing SD card...");
-
-  if (!SD.begin()) {
-    Serial.println("initialization failed!");
+  if (!SD.begin(chipSelect)) {
+    Serial.println("SD Card initialization failed!");
     return;
   }
-  Serial.println("initialization done.");
-
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  myFile = SD.open("test.txt", FILE_WRITE);
-
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.println("testing 1, 2, 3.");
-    // close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
-
-  // re-open the file for reading:
-  myFile = SD.open("test.txt");
-  if (myFile) {
-    Serial.println("test.txt:");
-
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-    }
-    // close the file:
-    myFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
+  Serial.println("SD Card initialized.");
 }
 
-void loop()
-{
-  // nothing happens after setup
+void loop() {
+  if (Serial.available() > 0) {
+    String receivedData = Serial.readStringUntil('\n');
+    myFile = SD.open("data_log.csv", FILE_WRITE);
+    if (myFile) {
+      myFile.println(receivedData);
+      myFile.close();
+    } else {
+      Serial.println("Error opening data_log.csv");
+    }
+  }
 }
